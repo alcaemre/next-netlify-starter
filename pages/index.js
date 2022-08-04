2659670b7db44c6cfc2dde223fe0068329647e18
alcaemre <emre.alca@icloud.com>
2022-08-03 20:33:49 -0400

    test commit

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="hello world!" />
+        <Header title="hello world! test" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
